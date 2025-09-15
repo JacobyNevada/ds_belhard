0e06197523c8bfd7ee1cb44294ccee048fb83058
JacobyNevada <v.khlapianik@gmail.com>
2025-09-15 22:29:16 +0300

    Update data_informer.py

--- hw2/data_informer.py
+++ hw2/data_informer.py
@@ -1,34 +1,35 @@
 import pandas as pd
 
 class Data_Informer:
+  @staticmethod
   def show_dataset(dataset):
     print(f"\nDataset head:\n{dataset}\n")
 
-
+  @staticmethod
   def show_info(dataset):
     print(f"\nDataset info:\n{dataset.info()}\n")
 
-
+  @staticmethod
   def show_describe(dataset):
     print(f"\nDataset describe:\n{dataset.describe()}\n")
 
-
+  @staticmethod
   def show_types(dataset):
     print(f"\nDataset types:\n{dataset.dtypes}\n")
 
-
+  @staticmethod
   def show_null_values(dataset):
     print(f"\nDataset null values:\n{dataset.isnull().sum()}\n")
 
-
+  @staticmethod
   def show_nan_values(dataset):
     print(f"\nDataset NaN values:\n{dataset.isna().sum()}\n")
 
-
+  @staticmethod
   def show_all_info(dataset):
-    show_dataset(dataset)
-    show_info(dataset)
-    show_describe(dataset)
-    show_types(dataset)
-    show_null_values(dataset)
-    show_nan_values(dataset)
+    Data_Informer.show_dataset(dataset)
+    Data_Informer.show_info(dataset)
+    Data_Informer.show_describe(dataset)
+    Data_Informer.show_types(dataset)
+    Data_Informer.show_null_values(dataset)
+    Data_Informer.show_nan_values(dataset)
