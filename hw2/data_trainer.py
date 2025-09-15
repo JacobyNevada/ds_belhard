ad0a853136e99c1f6532c9462f1d8936d4f7c484
JacobyNevada <v.khlapianik@gmail.com>
2025-09-15 22:30:14 +0300

    Update data_trainer.py

--- hw2/data_trainer.py
+++ hw2/data_trainer.py
@@ -4,16 +4,17 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
 class Data_Trainer:
+  @staticmethod
   def train_model(X, y):
     model = LogisticRegression(solver='lbfgs', max_iter=1000)
     model.fit(X, y)
     return model
 
-
+  @staticmethod
   def predict(model, X):
     return model.predict(X)
 
-
+  @staticmethod
   def evaluate_model(y_true, y_pred):
     accuracy = accuracy_score(y_true, y_pred)
     report = classification_report(y_true, y_pred)
